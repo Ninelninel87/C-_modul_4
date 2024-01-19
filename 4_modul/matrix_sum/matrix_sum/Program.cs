71a4169a91d8d2bc2fbe5a194093c5f137d7519e
Ninelninel87 <135541573+Ninelninel87@users.noreply.github.com>
2024-01-19 10:35:38 +0300

    Add files via upload

---
+++ 4_modul/matrix_sum/matrix_sum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите кол-во рядов: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите кол-во колонок: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] matrix1 = new int[rows, columns];
+        int[,] matrix2 = new int[rows, columns];
+        int[,] sumMatrix = new int[rows, columns];
+        Random random = new Random();
+
+        Console.WriteLine("Первая матрица: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix1[i, j] = random.Next(1, 100);
+                Console.Write(matrix1[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Вторая матрица: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix2[i, j] = random.Next(1, 100);
+                Console.Write(matrix2[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Сумма матриц: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                sumMatrix[i, j] = matrix1[i, j] + matrix2[i, j];
+                Console.Write(sumMatrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 4_modul/random_matrix/random_matrix/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество рядов: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество колонок: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, columns];
+        Random random = new Random();
+
+        Console.WriteLine("Матрица: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                matrix[i, j] = random.Next(1, 100); 
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                sum += matrix[i, j];
+            }
+        }
+
+        Console.WriteLine("Сумма всех элементов: " + sum);
+    }
+}
+
